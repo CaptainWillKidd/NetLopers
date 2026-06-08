d22460e41589af22785be0bfc64f6501f035af61
Holly Briggs <hbriggs@byupathway.edu>
2026-06-08 17:55:39 -0600

    Added validation and durationminutes to the activity form and removed editing and adding images to the edit page

--- SunsetSchedule/Services/ActivityService.cs
+++ SunsetSchedule/Services/ActivityService.cs
@@ -1,19 +1,16 @@
 using SunsetSchedule.Data;
 using SunsetSchedule.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Components.Forms;
 
 namespace SunsetSchedule.Services;
 
 public class ActivityService
 {
     private readonly ApplicationDbContext _db;
-    private readonly ImageService _imageService;
 
-    public ActivityService(ApplicationDbContext db, ImageService imageService)
+    public ActivityService(ApplicationDbContext db)
     {
         _db = db;
-        _imageService = imageService;
     }
 
     public async Task<List<Activity>> GetAllAsync()
@@ -34,54 +31,25 @@ public class ActivityService
     {
         _db.Activities.Add(activity);
         await _db.SaveChangesAsync();
-
         return activity;
     }
 
-    public async Task UpdateAsync(Activity activity, IBrowserFile? mainImage, IReadOnlyList<IBrowserFile>? gallery)
+    public async Task UpdateAsync(Activity activity)
     {
         _db.Activities.Update(activity);
-        await _db.SaveChangesAsync();
-
-        // MAIN IMAGE
-        if (mainImage is not null)
-        {
-            var path = await _imageService.SaveImageAsync(mainImage, "wwwroot/images/uploads");
-
-            _db.ActivityImages.Add(new ActivityImage
-            {
-                ActivityId = activity.Id,
-                ImagePath = path,
-                DisplayOrder = 0
-            });
-        }
-
-        // GALLERY IMAGES
-        if (gallery is not null)
-        {
-            int order = 1;
-
-            foreach (var file in gallery)
-            {
-                var path = await _imageService.SaveImageAsync(file, "wwwroot/images/uploads");
-
-                _db.ActivityImages.Add(new ActivityImage
-                {
-                    ActivityId = activity.Id,
-                    ImagePath = path,
-                    DisplayOrder = order++
-                });
-            }
-        }
-
         await _db.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(int id)
     {
-        var activity = await _db.Activities.FindAsync(id);
+        var activity = await _db.Activities
+            .Include(a => a.Images)
+            .FirstOrDefaultAsync(a => a.Id == id);
 
         if (activity is null) return;
+
+        // optional cleanup if you want to keep DB clean
+        _db.ActivityImages.RemoveRange(activity.Images);
 
         _db.Activities.Remove(activity);
 
